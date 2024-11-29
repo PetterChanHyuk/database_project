b6b64a0d9806db4f7940eb2f6d4d7339ce98a2fe
PetterChanHyuk <pchishahaboy@naver.com>
2024-11-29 22:20:04 +0900

    updated

--- backend/resist/models.py
+++ backend/resist/models.py
@@ -1,4 +1,4 @@
-#backend/resist/models.py
+# backend/resist/models.py
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -22,12 +22,12 @@ class User(AbstractUser):
 # Classroom model
 class Classroom(models.Model):
     room_id = models.CharField(max_length=50, primary_key=True)
-    building_name = models.CharField(max_length=100, null=False, default = "신공학관")
+    building_name = models.CharField(max_length=100, null=False, default="신공학관")
     room_number = models.IntegerField(null=False)
     capacity = models.IntegerField(default=1)
     is_checked = models.BooleanField(default=False)
-    has_air_conditioning = models.BooleanField()
-    has_lighting = models.BooleanField()
+    has_air_conditioning = models.BooleanField(default=False)
+    has_lighting = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.building_name} {self.room_number}"
@@ -38,8 +38,8 @@ class Reservation(models.Model):
     applicant = models.ForeignKey(User, on_delete=models.CASCADE)
     tag_1 = models.CharField(max_length=50, blank=True, null=True)
     tag_2 = models.CharField(max_length=50, blank=True, null=True)
-    custum_tag = models.CharField(max_length=50, blank=True, null=True)
-    reservation_date = models.CharField(max_length=50, null=True, blank=True)
+    custom_tag = models.CharField(max_length=50, blank=True, null=True)
+    reservation_date = models.DateField(null=True, blank=True)
     start_time = models.DateTimeField(null=False)
     end_time = models.DateTimeField(null=False)
     attendees_count = models.IntegerField(null=True, blank=True)
@@ -50,11 +50,11 @@ class Reservation(models.Model):
 # Course model
 class Course(models.Model):
     course_id = models.CharField(max_length=50, primary_key=True)
-    course_name = models.TextField(max_length=50, blank=True, null=True)
-    professor_name = models.TextField(max_length=50, blank=True, null=True)
+    course_name = models.CharField(max_length=50, blank=True, null=True)
+    professor_name = models.CharField(max_length=50, blank=True, null=True)
     tag_1 = models.CharField(max_length=50, blank=True, null=True)
     tag_2 = models.CharField(max_length=50, blank=True, null=True)
-    custum_tag = models.CharField(max_length=50, blank=True, null=True)
+    custom_tag = models.CharField(max_length=50, blank=True, null=True)
     classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE)
     day_of_week = models.CharField(max_length=50, null=True, blank=True)
     start_time = models.DateTimeField(null=False)
@@ -63,32 +63,33 @@ class Course(models.Model):
     def __str__(self):
         return f"Course {self.course_id} in {self.classroom}"
 
-class timecheck(models.Model):
-    _1 = models.BooleanField(default = True)
-    _1h = models.BooleanField(default = True)
-    _2 = models.BooleanField(default = True)
-    _2h = models.BooleanField(default = True)
-    _3 = models.BooleanField(default = True)
-    _3h = models.BooleanField(default = True)
-    _4 = models.BooleanField(default = True)
-    _4h = models.BooleanField(default = True)
-    _5 = models.BooleanField(default = True)
-    _5h = models.BooleanField(default = True)
-    _6 = models.BooleanField(default = True)
-    _6h = models.BooleanField(default = True)
-    _7 = models.BooleanField(default = True)
-    _7h = models.BooleanField(default = True)
-    _8 = models.BooleanField(default = True)
-    _8h = models.BooleanField(default = True)
-    _9 = models.BooleanField(default = True)
-    _9h = models.BooleanField(default = True)
-    _10 = models.BooleanField(default = True)
-    _10h = models.BooleanField(default = True)
-    _11 = models.BooleanField(default = True)
-    _11h = models.BooleanField(default = True)
-    _12 = models.BooleanField(default = True)
-    _12h = models.BooleanField(default = True)
-    _13 = models.BooleanField(default = True)
-    _13h = models.BooleanField(default = True)
-    _14 = models.BooleanField(default = True)
-    _14h = models.BooleanField(default = True)
+# TimeCheck model
+class TimeCheck(models.Model):
+    _1 = models.BooleanField(default=True)
+    _1h = models.BooleanField(default=True)
+    _2 = models.BooleanField(default=True)
+    _2h = models.BooleanField(default=True)
+    _3 = models.BooleanField(default=True)
+    _3h = models.BooleanField(default=True)
+    _4 = models.BooleanField(default=True)
+    _4h = models.BooleanField(default=True)
+    _5 = models.BooleanField(default=True)
+    _5h = models.BooleanField(default=True)
+    _6 = models.BooleanField(default=True)
+    _6h = models.BooleanField(default=True)
+    _7 = models.BooleanField(default=True)
+    _7h = models.BooleanField(default=True)
+    _8 = models.BooleanField(default=True)
+    _8h = models.BooleanField(default=True)
+    _9 = models.BooleanField(default=True)
+    _9h = models.BooleanField(default=True)
+    _10 = models.BooleanField(default=True)
+    _10h = models.BooleanField(default=True)
+    _11 = models.BooleanField(default=True)
+    _11h = models.BooleanField(default=True)
+    _12 = models.BooleanField(default=True)
+    _12h = models.BooleanField(default=True)
+    _13 = models.BooleanField(default=True)
+    _13h = models.BooleanField(default=True)
+    _14 = models.BooleanField(default=True)
+    _14h = models.BooleanField(default=True)
